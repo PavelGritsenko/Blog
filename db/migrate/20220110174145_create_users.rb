7e9f23436ff44db8f844b9d4696e8c70def46f36
PavelGritsenko <pasha.zp.ua94@gmail.com>
2022-01-14 01:27:52 +0200

    migrate to postgresql

--- db/migrate/20220110174145_create_users.rb
+++ db/migrate/20220110174145_create_users.rb
@@ -3,9 +3,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email, null: false, index: { unique: true }
-      t.string :name
-      t.string :password_digest
+      t.text :email, null: false, index: { unique: true }
+      t.text :name
+      t.text :password_digest
 
       t.timestamps
     end
